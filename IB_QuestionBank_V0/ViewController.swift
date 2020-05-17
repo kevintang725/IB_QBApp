7e71a27720a06f383547b4a45bab3c7cf0d1cc17
Kevin Tang <kwkt725@gmail.com>
2020-05-17 17:36:30 +0800

    Initial Commit

---
+++ IB_QuestionBank_V0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IB_QuestionBank_V0
+//
+//  Created by Kevin Tang on 17/5/2020.
+//  Copyright © 2020 Kevin Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
